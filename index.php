d9c6fe9de018b73f30ff5980bd203cce03afb4ec
anton <sterh2222@mail.ru>
2017-01-13 23:32:05 +0500

    index and style

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="styles/style.css">
+	<title>Test site</title>
+</head>
+<body>
+	<div class="wrap">
+		<header class="clearfix">
+			<div class="logo">MySite</div>
+			<div class="descr">This is my new site. I have did it. Come here everybody</div>
+		</header>
+		<main class="clearfix">
+			<aside>
+				<ul class="menu">
+					<li><a href="#">Link1</a></li>
+					<li><a href="#">Link2</a></li>
+					<li><a href="#">Link3</a></li>
+				</ul>
+			</aside>
+			<div class="content">
+				<h2>Header contents</h2>
+				<section>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, sapiente!</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus officia est, quisquam veniam praesentium. Numquam non itaque, repellat dolorum nobis.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit repudiandae repellendus enim tempora, temporibus voluptas, iure ut. Expedita, veritatis suscipit illum repellat doloribus, culpa officia mollitia iusto animi ea unde.</p>
+				</section>
+			</div>
+		</main>
+		<footer>
+			<p>Copyright my site.</p>
+		</footer>
+	</div>
+</body>
+</html>
