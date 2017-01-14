118f66dbb5a1a544f5f5132ba8c9e1d180c93aa4
anton <sterh2222@mail.ru>
2017-01-14 12:39:02 +0500

    add admin/index, style, inc, secure

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<title>Admin</title>
+</head>
+<body>
+	<ul>
+		<li><a href="#">Save</a></li>
+		<li><a href="#">Edit</a></li>
+		<li><a href="#">Logout</a></li>
+	</ul>
+</body>
+</html>
